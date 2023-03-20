878d4ad44ba24f926ca08d2b3a1f60f5144ccf3e
Mark Wilkinson <markw@illuminae.com>
2023-03-20 09:00:19 +0100

    bootstrap gem

---
+++ lib/lineage_block/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module LineageBlock
+  VERSION = "0.1.0"
+end
---
+++ spec/lineage_block_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe LineageBlock do
+  it "has a version number" do
+    expect(LineageBlock::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
